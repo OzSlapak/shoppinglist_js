4f60b68729cb6b0356e9a58ce5b9ce8d0c54e770
OzSlapak <ozgeslapak@gmail.com>
2022-09-10 12:39:28 -0400

    ::before cleaned from button

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ submit.addEventListener("click",function(e) {
     const product = document.createElement("div");
     product.classList.add("shopping-list");
     product.innerHTML = `<div class="shopping-list">
-    <article class="shopping-item">
-        <p class="title">${item}</p>
-        <div class="btn-container">
-            <button type="button" class="edit-btn"></button>
-            <button type="button" class="delete-btn">
-            <i class="fas fa-trash">::before</i>               
-            </button>
-        </div>
-    </article>
-</div>`
+        <article class="shopping-item">
+            <p class="title">${item}</p>
+            <div class="btn-container">
+                <button type="button" class="edit-btn"></button>
+                <button type="button" class="delete-btn">
+                    <i class="fas fa-trash"></i>               
+                </button>
+            </div>
+        </article>
+    </div>`
     const itemList = document.querySelector(".shopping-list")
     itemList.appendChild(product)
     alert("Your product is added") 
